b4da268830218e3530c3f206dcc87d86402ecee4
vianna-dev <gabrielvianna.dev@Outloook.com>
2023-03-20 21:14:19 -0300

    clima-tempo.JS

---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+// variaveis e seleçao de elementos
+const apiKey = "504d9c975d3d1835f90e2f2fd54c9fce";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const humidityElement = document.querySelector("#humidity span");
+const windElement = document.querySelector("#wind span");
+
+const weatherContainer = document.querySelector("#weather-data");
+
+// Funçoes
+const getWeatherData = async(city) => {
+
+    const apiWatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
+
+    const res = await fetch(apiWatherURL)
+    const data = await   res.json()
+
+    return data
+};
+
+const showWeatherData = async (city) => {
+  const data = await getWeatherData(city);
+
+  cityElement.innerText = data.name;
+  tempElement.innerText = parseInt(data.main.temp)
+  descElement.innerText = data.weather[0].description;
+  weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+  );
+  countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+  humidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed}km/h`;
+
+  weatherContainer.classList.remove("hide");
+};
+
+// Eventos
+searchBtn.addEventListener("click", (e) => {    
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+
+    if(e.code === "Enter") {
+       const city = e.target.value
+
+       showWeatherData(city);
+    }
+});
